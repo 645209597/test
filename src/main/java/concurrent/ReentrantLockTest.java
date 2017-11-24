043847f5dd13eaeb929426b921cf04e2233e20b4
qingfeng <qingfeng@tongbanjie.com>
2017-11-24 14:38:44 +0800

    no message

--- src/main/java/concurrent/ReentrantLockTest.java
+++ src/main/java/concurrent/ReentrantLockTest.java
@@ -2,6 +2,11 @@ package concurrent;
 
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * 可重入锁是指持有锁的线程可以再次持有同一把锁，且释放锁的次数等于加锁次数时才能释放锁
+ * 
+ * @author qingfeng
+ */
 public class ReentrantLockTest {
 
 	public static void main(String[] args) {
@@ -9,6 +14,9 @@ public class ReentrantLockTest {
 		lock.lock();
 		lock.lock();
 		System.out.println("a");
+		lock.unlock();
+		lock.unlock();
+		lock.unlock();
 		
 		new Thread(new Runnable() {
 
