7bb9b84908ec58c8b4c2a06772ea12ae7b9b79fb
qingfeng <qingfeng@tongbanjie.com>
2017-11-24 10:09:54 +0800

    no message

--- src/main/java/concurrent/ReentrantLockTest.java
+++ src/main/java/concurrent/ReentrantLockTest.java
@@ -5,14 +5,13 @@ import java.util.concurrent.locks.ReentrantLock;
 public class ReentrantLockTest {
 
 	public static void main(String[] args) {
-		ReentrantLock lock = new ReentrantLock();
+		final ReentrantLock lock = new ReentrantLock();
 		lock.lock();
 		lock.lock();
 		System.out.println("a");
 		
 		new Thread(new Runnable() {
 
-			@Override
 			public void run() {
 				lock.lock();
 				System.out.println("b");
