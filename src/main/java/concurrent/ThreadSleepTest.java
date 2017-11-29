b7e2eef53ac7122088951e87da96b0bdee6fcda3
qingfeng <645209597@qq.com>
2017-11-30 00:48:29 +0800

    no message

--- src/main/java/concurrent/ThreadSleepTest.java
+++ src/main/java/concurrent/ThreadSleepTest.java
@@ -8,26 +8,25 @@ package concurrent;
 public class ThreadSleepTest {
 
 	public static void main(String[] args) {
-		final Thread c = Thread.currentThread();
-		
-		new Thread(new Runnable() {
+		Thread t = new Thread(new Runnable() {
 			@Override
 			public void run() {
 				try {
-					Thread.sleep(1000);
+					Thread.sleep(10000);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-				
-				System.out.println(c.getState());
 			}
-		}).start();
+		});
+		t.start();
 		
 		try {
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		
+		System.out.println(t.getState());
 	}
 
 }
