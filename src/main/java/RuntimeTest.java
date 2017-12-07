9b4f9b23d6abb6f0d2f606c01f2f791ef4559d2c
qingfeng <qingfeng@tongbanjie.com>
2017-12-07 21:50:39 +0800

    no message

--- src/main/java/RuntimeTest.java
+++
@@ -1,9 +0,0 @@
-public class RuntimeTest {
-
-	public static void main(String[] args) {
-		System.out.println(Runtime.getRuntime().maxMemory());
-		System.out.println(Runtime.getRuntime().freeMemory());
-		System.out.println(Runtime.getRuntime().totalMemory());
-	}
-
-}
